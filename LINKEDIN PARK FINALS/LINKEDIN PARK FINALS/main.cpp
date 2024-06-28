d0ef11aa4820bb38cc6556e258cdd20ff6ff556a
BroTakuGH <93759335+BroTakuGH@users.noreply.github.com>
2024-06-28 22:44:51 +0800

    Add files via upload

---
+++ LINKEDIN PARK FINALS/LINKEDIN PARK FINALS/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+//test1
+int main()
+{
+    int input;
+    cout << "===========================================\n";
+    cout << "\t\t TITLE\n";
+    cout << "===========================================\n";
+    cout << "[1] New Video\n";
+    cout << "[2] Rent a Video\n";
+    cout << "[3] Return a Video\n";
+    cout << "[4] Show Video Details\n";
+    cout << "[5] Display all Videos\n";
+    cout << "[6] Check Video Availability\n";
+    cout << "[7] Customer Maintenance\n";
+    cout << "[8] Exit Program\n";
+    cout << "===========================================\n";
+
+    cin >> input;
+
+    switch (input) {
+    case 1:
+
+        break;
+    case 2:
+
+        break;
+    case 3:
+
+        break;
+    case 4:
+
+        break;
+    case 5:
+
+        break;
+    case 6:
+
+        break;
+    case 7:
+
+        break;
+    case 8:
+
+        break;
+    }
+}
+
